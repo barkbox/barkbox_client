a7043480ff51d769f8bff8d6bec9a6d694f54c6f
Becky Segal <becsegal@gmail.com>
2017-02-20 20:45:06 -0500

    make auth available in controllers, accept all caps authorization header

--- app/helpers/barkbox_client/auth_helper.rb
+++ app/helpers/barkbox_client/auth_helper.rb
@@ -2,21 +2,27 @@ module BarkboxClient
   module AuthHelper
 
     def login!
-      auth = BarkboxClient.auth_class.login(params[:email], params[:password])
+      @auth = BarkboxClient.auth_class.login(params[:email], params[:password])
       raise BarkboxClient::UnauthenticatedError unless auth.present?
     end
 
     def authenticate!
-      auth = BarkboxClient.auth_class.verify(request_token)
+      @auth = BarkboxClient.auth_class.verify(request_token)
       raise BarkboxClient::UnauthenticatedError unless auth.present?
     end
 
     def request_token
       if request.env['Authorization'].present?
         return request.env['Authorization'].gsub('Bearer ', '')
+      elsif request.env['HTTP_AUTHORIZATION'].present?
+        return request.env['HTTP_AUTHORIZATION'].gsub('Bearer ', '')
       elsif params[:access_token].present?
         return params[:access_token]
       end
+    end
+
+    def auth
+      @auth
     end
 
   end
